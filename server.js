2297fbf4aebd73f65af446bf34f2059b1dfac10f
sgevm <sudeep.ghag.ext@gmail.com>
2022-07-06 20:49:28 +0000

    cookie secure

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(session({
     resave: false,
     saveUninitialized: true,
     cookie: { 
-      secure: false,
+      secure: process.env.COOKIE_SECURE,
       httpOnly: true,
       sameSite: 'none'
   }
